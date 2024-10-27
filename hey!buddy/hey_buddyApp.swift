d32c2a8b5f621a017c307664aa02702b4002846f
polina efimova <ffforward@bk.ru>
2024-10-27 17:51:47 +0300

    Initial Commit

---
+++ hey!buddy/hey_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hey_buddyApp.swift
+//  hey!buddy
+//
+//  Created by Полина Ефимова on 27.10.24.
+//
+
+import SwiftUI
+
+@main
+struct hey_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
